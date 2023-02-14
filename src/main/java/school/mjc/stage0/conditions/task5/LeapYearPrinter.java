e5a84cc01ef27b2fd895e2d0d8076e359d7a4373
Arman <gcom0733@gmail.com>
2023-02-14 18:34:41 +0600

    implemented methods

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,25 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeap = false;
+        switch (year % 4) {
+            case 0:
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        isLeap = true;
+                    }
+                } else {
+                    isLeap = true;
+                }
+                break;
+            default:
+                isLeap = false;
+                break;
+        }
+        if (isLeap) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
